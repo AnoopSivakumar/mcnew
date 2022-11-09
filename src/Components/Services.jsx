012ec4cecb5ea82892cec49dfdce9f3a3ae03ba5
Anoop Sivakumar <91863713+AnoopSivakumar@users.noreply.github.com>
2022-11-09 13:35:56 +0530

    Update Services.jsx

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -20,22 +20,7 @@ const Services = () => {
       </div>
 
       <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-        {
-        videos.map((obj,index) => (
-          <div key={index} className=" shadow-md shadow-gray-600 rounded-lg">
-            <img
-              src={obj.thumbnails}
-              alt=""
-              className="rounded-md duration-200 hover:scale-105"
-            />
-            <div className="flex items-center justify-center">
-              <button className=" w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-              {obj.title}
-              </button>
-             
-            </div>
-          </div>
-        ))}
+        
       </div>
     </div>
   </div>
